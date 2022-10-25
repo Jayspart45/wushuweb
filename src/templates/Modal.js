83177e9c35df0ecdcd4ef26e4a9eb56a6501b45e
Jayspart45 <91474610+Jayspart45@users.noreply.github.com>
2022-10-25 13:10:04 +0530

    Update Modal.js

--- src/templates/Modal.js
+++ src/templates/Modal.js
@@ -2,6 +2,20 @@ import React from 'react';
 import image1 from './images/img1.jpeg';
 
 export default function Modal() {
+    let image = document.querySelectorAll('.card img');
+    let imgsrc;
+    let modalsrc = document.querySelector('.modal-body img');
+
+    image.forEach((img) => {
+        img.addEventListener('click', (e) => {
+            imgsrc = e.target.src;
+
+            // console.log(imgsrc);
+            // console.log(modalsrc.src);
+            modalsrc.src = imgsrc;
+        });
+    });
+
     return (
         <div
             className="modal fade"
@@ -14,7 +28,7 @@ export default function Modal() {
             <div className="modal-dialog  modal-dialog-centered modal-lg" role="document">
                 <div className="modal-content">
                     <div className="modal-body p-0">
-                        <img src={image1} alt="" />
+                        <img src={imgsrc} alt="" />
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-dismiss="modal">
